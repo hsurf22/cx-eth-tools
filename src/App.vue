4873c99ec69ac1672377488afb16c99d6b668cbc
David Hong <hsurf22@gmail.com>
2022-03-28 18:03:56 -0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Web3 from 'web3';
+
+const web3 = new Web3(Web3.givenProvider || 'ws://localhost:8545');
+
+console.log(web3);
+</script>
+
+<template>
+  <h1>Hi</h1>
+</template>
+
+<script></script>
+
+<style scoped lang="scss">
+h1 {
+  color: red;
+}
+</style>
