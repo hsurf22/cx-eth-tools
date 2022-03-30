6839fa05f9e3fca17ff2e54b11204c4e97466018
David Hong <hsurf22@gmail.com>
2022-03-30 06:29:13 -0700

    update wallet page

--- src/components/PageWallet/PageWallet.vue
+++ src/components/PageWallet/PageWallet.vue
@@ -11,37 +11,89 @@ defineProps({
 
 <template>
   <div>
-    <div>Wallet information</div>
+    <h2>Wallet information</h2>
+    <table>
+      <thead></thead>
+      <tbody>
+        <tr>
+          <td>Network</td>
+          <td>{{ network.name }}</td>
+        </tr>
+        <tr>
+          <td>Balance(Eth)</td>
+          <td>{{ balance }}</td>
+        </tr>
+        <tr>
+          <td>Sent transaction count</td>
+          <td>{{ txCount }}</td>
+        </tr>
+        <tr>
+          <td>ENS address for this wallet</td>
+          <td>{{ ensAddress }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 import { ethers } from 'ethers';
+import { defineComponent } from 'vue';
+//import { mapState } from 'pinia';
+import { useSettingsStore } from '@/stores/settings';
 
 const provider = new ethers.providers.JsonRpcProvider(
   'https://rpc.ankr.com/eth'
 );
 
-//const signer = provider.getSigner();
-
-//const balance = await provider.getBalance('ethers.eth');
-
-//console.log(provider);
-
-export default {
+export default defineComponent({
   data() {
-    return {};
+    return {
+      settingsStore: null,
+      ethAddress: '',
+      network: '',
+      balance: '',
+      txCount: 0,
+      ensAddress: null
+    };
   },
-  methods: {},
-  mounted() {}
-};
+  computed: {
+    //...mapState(useSettingsStore, ['ethAddress'])
+    /* async balance() {
+      const balance = await provider.getBalance('ricmoo.eth');
+      console.log(balance);
+
+      return balance;
+    } */
+  },
+  watch: {},
+  methods: {
+    async loadWalletInfo() {
+      const balance = await provider.getBalance(this.ethAddress);
+      const balanceInEth = ethers.utils.formatEther(balance);
+      this.balance = balanceInEth;
+
+      this.txCount = await provider.getTransactionCount(this.ethAddress);
+
+      this.ensAddress = await provider.lookupAddress(this.ethAddress);
+
+      this.network = await provider.getNetwork();
+    }
+  },
+  async mounted() {
+    this.settingsStore = await useSettingsStore();
+    this.ethAddress = this.settingsStore.ethAddress;
+    await this.loadWalletInfo();
+  }
+});
 </script>
 
 <style
   scoped
   lang="scss"
 >
-div {
-  font-size: 30px;
+table td {
+  border: 1px solid grey;
+  padding: 0 5px;
 }
 </style>
